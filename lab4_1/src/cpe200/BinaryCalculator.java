be5f1707bcff254ab5a04d1a16717b2670b7bac8
sofia <sofia_sjs@hotmail.com>
2016-09-14 00:10:03 +0700

    sent

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,98 @@
 package cpe200;
 
 
+import java.text.DecimalFormat;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = operand;
     }
 
-
     public void setSecondOperand(Operand operand) {
+        secondOperand = operand;
     }
 
     public String add() {
-        return null;
+        double total = Double.parseDouble(firstOperand.getOperand()) + Double.parseDouble(secondOperand.getOperand());
+        if( (total == Math.floor(total) && !Double.isInfinite(total))){
+            int i = (int)total;
+            return Integer.toString(i);
+        }else {
+            return Double.toString(total);
+        }
+
     }
 
     public String subtract() {
-        return null;
+        double total = Double.parseDouble(firstOperand.getOperand()) - Double.parseDouble(secondOperand.getOperand());
+        if( (total == Math.floor(total) && !Double.isInfinite(total))){
+            int i = (int)total;
+            return Integer.toString(i);
+        }else {
+            DecimalFormat formatter = new DecimalFormat("#0.0");
+            return formatter.format(total);
+
+        }
+
     }
 
     public String multiply() {
-        return null;
+        double total = Double.parseDouble(firstOperand.getOperand()) * Double.parseDouble(secondOperand.getOperand());
+        if( (total == Math.floor(total) && !Double.isInfinite(total))){
+            int i = (int)total;
+            return Integer.toString(i);
+        }else {
+            DecimalFormat formatter = new DecimalFormat("#0.0");
+            return formatter.format(total);
+            //Math.round(total,1);
+        }
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        if(Double.parseDouble(secondOperand.getOperand()) != 0) {
+            double total = Double.parseDouble(firstOperand.getOperand()) / Double.parseDouble(secondOperand.getOperand());
+            if ((total == Math.floor(total) && !Double.isInfinite(total))) {
+                int i = (int) total;
+                return Integer.toString(i);
+
+            } else {
+                total = total*100000;
+                total = Math.floor(total);
+               int count = (int)total;
+                   total = (double)count/100000;
+                return Double.toString(total);
+
+            }
+        }else{
+            int a=2;
+            return Integer.toString(a/0);
+        }
+
     }
 
     public String power() {
-        return null;
+        double firs,second;
+        double total;
+        firs = Double.parseDouble(firstOperand.getOperand());
+        second = Double.parseDouble(secondOperand.getOperand());
+        total = Math.pow(firs,second);
+        if( (total == Math.floor(total) && !Double.isInfinite(total))){
+            int i = (int)total;
+            return Integer.toString(i);
+        }else {
+            DecimalFormat formatter = new DecimalFormat("#0.0");
+            return formatter.format(total);
+            //Math.round(total,1);
+        }
+
     }
+
 
 }
--- lab4_2/src/cpe200/Users.java
+++ lab4_2/src/cpe200/Users.java
@@ -6,44 +6,71 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
+    public Users() {
+
+        userList = new ArrayList<User>();
+
+    }
     public ArrayList<User> userList;
+    User usertemp;
 
     public void addUser(User user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        usertemp.setUserName(userName);
+        usertemp.setPassword(password);
+        userList.add(usertemp);
     }
 
     public void deleteUser(User user)
     {
-
+        userList.remove(user);
     }
 
     public boolean exists(User user)
     {
-        return false;
+        if(userList.contains(user)){
+            return true;
+        }else {
+            return false;
+        }
     }
 
     public boolean usernameExists(String username)
     {
-        return false;
+        if(userList.contains(username)){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     /* This method should return null when the user with username is not in the list */
     public User getUserByUsername(String userName)
     {
+        for(int i = 0; i < count(); i++){
+            if(userList.get(i).userName == userName ){
+                return userList.get(i);
+            }
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
-    public User[] getUserArray()
-    {
-        return null;
+    public User[] getUserArray() {
+        User[] arr = new User[count()];
+        for (int i = 0; i < count(); i++)
+            arr[i] = userList.get(i);
+        return arr;
+
+
     }
 }
